293b8f12166471e1f636dd23022747f66943c036
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-01-24 16:58:57 +0100

    L2

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Commit A");
         System.out.println("Commit B");
         System.out.println("modificacion lider 1");
+        System.out.println("modificacion lider 2");
     }
 }
